7d5655780249b9e0a31adf85e9899c840dc927a3
Mariga123 <johnmariga8@gmail.com>
2020-11-15 15:44:00 +0300

    craete a link function to call the git hub repos

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../service/github.service';
+import { User } from '../user';
+import { Repos } from '../repos';
 
 @Component({
   selector: 'app-repository',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoryComponent implements OnInit {
 
-  constructor() { }
+user: User;
+repo: Repos;
 
-  ngOnInit(): void {
+  constructor(public githubService: GithubService) { }
+
+  // tslint:disable-next-line: typedef
+  getRepo(searchName){
+    this.githubService.getUser(searchName).then(
+      (success) => {
+        this.user = this.githubService.user;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.githubService.getRepo(searchName).then(
+      (success) => {
+        this.repo = this.githubService.repo;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    this.getRepo('Mariga123');
   }
 
 }
