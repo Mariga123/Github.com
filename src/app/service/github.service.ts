5c3f90fd17127470e99a30935aa0bdfbb22621ac
Mariga123 <johnmariga8@gmail.com>
2020-11-15 14:54:52 +0300

    create a service constructor to get repo and user name

--- src/app/service/github.service.ts
+++ src/app/service/github.service.ts
@@ -1,10 +1,19 @@
 import { environment } from './../../environments/environment.prod';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
+import { Repos } from '../repos';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+user: User;
+repo: Repos;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.user = new User('', '', '', '', '', '', '', '', new Date());
+    this.repo = new Repos('', '', '', '', '');
+   }
 }
