6a2b39ded25c0e536e69c540082176702019b17f
Mariga123 <johnmariga8@gmail.com>
2020-11-15 12:50:09 +0300

    create a constructor for class repo and user

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,2 +1,12 @@
 export class Repos {
+    constructor(
+        // tslint:disable-next-line: variable-name
+        public html_url: any,
+        public name: any,
+        // tslint:disable-next-line: variable-name
+        public repos_url: any,
+        public description: any,
+        // tslint:disable-next-line: variable-name
+        public forks_count: any,
+    ){}
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,17 @@
 export class User {
+    constructor(
+    // tslint:disable-next-line: variable-name
+    public public_repos: any,
+    public login: any,
+    // tslint:disable-next-line: variable-name
+    public html_url: any,
+    public name: any,
+    // tslint:disable-next-line: variable-name
+    public avatar_url: any,
+    public followers: any,
+    public following: any,
+    // tslint:disable-next-line: variable-name
+    public repos_url: any,
+    public created: Date,
+    ){}
 }
